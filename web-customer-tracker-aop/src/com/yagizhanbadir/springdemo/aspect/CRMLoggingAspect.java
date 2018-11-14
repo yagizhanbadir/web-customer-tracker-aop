9b5eb7193f30f8a643327301a81e9747e2a0f631
Yağızhan Badir <yagizhanbadir@gmail.com>
2018-11-14 19:48:07 +0300

    aop

---
+++ web-customer-tracker-aop/src/com/yagizhanbadir/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,68 @@
+package com.yagizhanbadir.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+
+	// setup pointcut declarations
+	@Pointcut("execution(* com.yagizhanbadir.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {
+	}
+
+	// do the same for service and dao
+	@Pointcut("execution(* com.yagizhanbadir.springdemo.service.*.*(..))")
+	private void forServicePackage() {
+	}
+
+	@Pointcut("execution(* com.yagizhanbadir.springdemo.dao.*.*(..))")
+	private void forDAOPackage() {
+	}
+
+	@Pointcut("forControllerPackage() || forServicePackage() || forDAOPackage()")
+	private void forAppFlow() {
+	}
+
+	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+
+		// display method we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====> in @Before: calling method: " + theMethod);
+
+		// display the arguments to the method
+
+		// get the arguments
+		Object[] args = theJoinPoint.getArgs();
+
+		// loop thru and display args
+		for (Object tempArg : args) {
+			myLogger.info("=====> argument: " + tempArg);
+		}
+	}
+
+	// add @AfterReturning advice
+	@AfterReturning(pointcut = "forAppFlow()", returning = "theResult")
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+
+		// display method we are returning form
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====> in @AfterReturning: from method: " + theMethod);
+		
+		// display data returned
+		myLogger.info("=====> result: " + theResult);
+	}
+
+}
---
+++ web-customer-tracker-aop/src/com/yagizhanbadir/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,51 @@
+package com.yagizhanbadir.springdemo.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import com.yagizhanbadir.springdemo.dao.CustomerDAO;
+import com.yagizhanbadir.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// new to inject customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+	@Override
+	@Transactional
+	public void saveCustomer(Customer theCustomer) {
+		customerDAO.saveCustomer(theCustomer);
+	}
+
+	@Override
+	@Transactional
+	public Customer getCustomer(int theId) {
+		return customerDAO.getCustomer(theId);
+	}
+
+	@Override
+	@Transactional
+	public void deleteCustomer(int theId) {
+		customerDAO.deleteCustomer(theId);
+	}
+
+	@Override
+	@Transactional
+	public List<Customer> searchCustomers(String theSearchName) {
+		return customerDAO.searchCustomers(theSearchName);
+	}
+
+}
